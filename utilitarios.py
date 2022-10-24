358be8f05201703a7ab54256b25664a777b22834
Allan Moreira <allan.vilacio@gmail.com>
2022-10-24 18:13:24 -0300

    criando funcao selecionar_palavra_aleatoria e arquivo requirements

--- utilitarios.py
+++ utilitarios.py
@@ -1,3 +1,5 @@
+import random
+from unidecode import unidecode
 
 def numero_jogador(msg='Digite o valor: '):
     passe = True
@@ -22,3 +24,14 @@ def deseja_continuar(msg='Valor invalido! Deseja continuar? [S/N]: '):
             return False
         elif continua =='S':
             return True
+
+def selecionar_palavra_aleatoria(file= 'frutas.txt', start=0, upper=True, remove_acento=True):
+    with open(file) as arquivo:
+        frutas = [fruta.strip() for fruta in arquivo]
+    rand = random.randrange(start, len(frutas))
+    fruta = frutas[random.randrange(start,len(frutas))]
+    if upper:
+        fruta = fruta.upper()
+    if remove_acento:
+        fruta = unidecode(fruta)
+    return fruta
